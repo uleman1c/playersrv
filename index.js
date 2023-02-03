d71b54ff498503dfa01fb385216972f26cf39495
Mihail <uleman@mail.ru>
2023-02-03 23:02:05 +0300

    create

--- index.js
+++ index.js
@@ -32,9 +32,31 @@
    * Use the main server root to
    * list all the books 
    */
+  app.get('/create', function (req, res) {
+  
+    db.run('CREATE TABLE files (id TEXT not null, name TEXT not null, ext TEXT not null)', (err,rows) => {
+
+        if (err) {
+            
+          res.send( err );
+        } else {
+            
+          res.send( 'ok' );
+        }
+
+        // 'SELECT name FROM sqlite_master WHERE type=\'table\''
+      //db.all(`SELECT * FROM files` , (err,rows) => {
+  
+          /**
+           * Send all the lines in the “book” table
+           */
+      });
+  
+  });
+  
   app.get('/', function (req, res) {
   
-      db.all(`SELECT * FROM files` , (err,rows) => {
+        db.all(`SELECT * FROM files` , (err,rows) => {
   
           /**
            * Send all the lines in the “book” table
@@ -48,18 +70,24 @@
    * Use this path to save, instead:
    * /save/ followed by title and author
    */
-  app.get('/save/:title/:author', function (req, res) {
+  app.get('/save', function (req, res) {
   
+        let filename = req.query.filename.split('.')
+        let ext = filename[filename.length - 1]
+        filename.splice(filename.length - 1, 1)
+
+        let name = filename.join('.')
+
       /**
        * Prepare the INSERT instruction in our table 
        */
-      var stmt = db.prepare("INSERT INTO books VALUES (?, ?)");
+      var stmt = db.prepare("INSERT INTO files VALUES (?, ?, ?)");
   
       /**
        * And run the query above, moving the data in the url
        * nell url 
        */
-      stmt.run( req.params.title, req.params.author , (err,rows) =>{
+      stmt.run( uuid.v4(), name, ext , (err,rows) =>{
   
           /**
            * Finally, send a ‘true’  status to show that 
